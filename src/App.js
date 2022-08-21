57f03a11fe70e2fa4ca2f57e9b855d690f2c1c32
Matias Sequeira <matias.sequeira@ioactive.com>
2022-08-20 20:32:32 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -166,14 +166,14 @@ function App() {
         </form>
       </div>
       <div>
-      // Mount the component in your code at the place where you want to render the widget
-      <WorldIDWidget
-        actionId="wid_BPZsRJANxct2cZxVRyh80SFG" // obtain this from developer.worldcoin.org
-        signal="my_signal"
-        enableTelemetry
-        onSuccess={(verificationResponse) => console.log(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
-        onError={(error) => console.error(error)}
-      />;
+        {/* Mount the component in your code at the place where you want to render the widget */}
+        <WorldIDWidget
+          actionId="wid_BPZsRJANxct2cZxVRyh80SFG" // obtain this from developer.worldcoin.org
+          signal="my_signal"
+          enableTelemetry
+          onSuccess={(verificationResponse) => console.log(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
+          onError={(error) => console.error(error)}
+        />;
       </div>
     </div>
 
